e01c70399c2cc6ff8e2f567fd28069983dbddf0a
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-07 13:22:43 -0700

    added different paths based on Route

--- src/containers/mainWindow.js
+++ src/containers/mainWindow.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import {Route, Switch} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
+
+import QCRecordWindow from '../window-components/qcRecordWindow'
+import RetainWindow from '../window-components/retainWindow'
+import StabilityWindow from '../window-components/stabilityWindow'
+import NotValid from '../window-components/notValid'
 
 export default class MainWindow extends React.Component {
     constructor() {
@@ -14,6 +20,12 @@ export default class MainWindow extends React.Component {
         return (
             <Container >
                 This is now the main window which will respond to the different switch api calls
+                <Switch>
+                    <Route exact path="/" component={QCRecordWindow} />
+                    <Route path="/retain" component={RetainWindow} />
+                    <Route path="/stability" component={StabilityWindow} />
+                    <Route component={NotValid} />
+                </Switch>
             </Container>
                 
         )
