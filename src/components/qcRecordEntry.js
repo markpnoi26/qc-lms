f5cea15a08190377dcd0aa4c11d5c45ded4325e1
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-07 12:06:48 -0700

    renamed file

--- src/components/qcRecordEntry.js
+++ src/components/qcRecordEntry.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-export default class QcEntry extends React.Component {
+export default class QcRecordEntry extends React.Component {
     constructor() {
         super()
         this.state={}
