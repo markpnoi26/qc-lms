a80c2c7fe0030c6eb92d13cb2cd8cfdcc5031f4d
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-06 23:11:40 -0700

    added base code for App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import NavBar from './containers/navBar'
+import SideBar from './containers/sideBar'
+import MainWindow from './containers/mainWindow'
+import {withAuthenticator} from '@aws-amplify/ui-react';
 
 const App = () => {
+
+  const appStyle={
+    display: "flex",
+    flexDirection: "column"
+  }
+
+  const barStyle={
+    flexDirection: "row"
+  }
+
   return (
-    <div>
-      <AmplifySignOut />
+    <div style={appStyle}>
+      <div>
+        <NavBar />
+      </div>
+      <div style={barStyle}>
+        <SideBar />
+        <MainWindow />
+      </div>
+
       QC-LMS
     </div>
   );
