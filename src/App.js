5bc66d5227be09e15cf06cf8e6765eafed85ed92
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-07 12:07:04 -0700

    added container wrapper for the application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-
+import {Container, Row, Col} from 'react-bootstrap'
 import NavBar from './containers/navBar'
 import SideBar from './containers/sideBar'
 import MainWindow from './containers/mainWindow'
 import {withAuthenticator} from '@aws-amplify/ui-react';
 
-class App {
+class App extends React.Component {
 
   render() {
     return (
-      <div >
-        <div>
-          <NavBar />
-        </div>
-        <div>
-          <SideBar />
-          <MainWindow />
-        </div>
-  
-        QC-LMS
-      </div>
+      <Container fluid >
+          <Row>
+            <NavBar />
+          </Row>
+          <Row>
+            <Col>
+              <SideBar />
+            </Col>
+            <Col xs={8}>
+              <MainWindow />
+            </Col>
+          </Row>
+      </Container>
     );
   }
 
