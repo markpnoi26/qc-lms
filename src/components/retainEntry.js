cec9c568271de7ec98a588328b70858473d99341
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-07 13:23:42 -0700

    adeed entry which will be used on different windows.

---
+++ src/components/retainEntry.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export default class RetainEntry extends React.Component {
+    constructor() {
+        super()
+        this.state={}
+    }
+
+    render() {
+        return(
+            <div>
+                I am a sample retain
+            </div>
+        )
+    }
+}
---
+++ src/components/stabilityEntry.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export default class StabilityEntry extends React.Component {
+    constructor() {
+        super()
+        this.state={}
+    }
+
+    render() {
+        return(
+            <div>
+                I am a stability retain
+            </div>
+        )
+    }
+}
