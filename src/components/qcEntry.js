d5d6b450604105eeb5a234c962ca27583f2b7d37
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-06 23:11:51 -0700

    added qc entry component

---
+++ src/components/qcEntry.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export default class QcEntry extends React.Component {
+    constructor() {
+        super()
+        this.state={}
+    }
+
+    render() {
+        return(
+            <div>
+                I am an entry QCblablabla
+            </div>
+        )
+    }
+}
