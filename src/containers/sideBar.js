799ae8e27e669fd3abc0e8d567a96b6d289afce0
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-14 21:34:29 -0700

    delted sidebar constructor

--- src/containers/sideBar.js
+++ src/containers/sideBar.js
@@ -2,12 +2,6 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 export default class SideBar extends React.Component {
-    constructor() {
-        super()
-        this.state={
-
-        }
-    }
 
     render() {
         return (
