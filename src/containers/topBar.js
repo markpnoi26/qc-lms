440737ed9e7c2eeb24507330fbc400897b722d51
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-14 21:34:41 -0700

    deleted sidebar constructor

--- src/containers/topBar.js
+++ src/containers/topBar.js
@@ -3,12 +3,6 @@ import {Col} from 'react-bootstrap'
 import { AmplifySignOut } from '@aws-amplify/ui-react';
 
 export default class TopBar extends React.Component {
-    constructor() {
-        super()
-        this.state={
-
-        }
-    }
 
     render() {
         
