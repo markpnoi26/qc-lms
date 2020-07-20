056241584d28568e623a5d97e22fe1bdfc5948af
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-20 13:17:27 -0700

    added fetch status

---
+++ src/reducers/fetchStatus.js
@@ -0,0 +1,12 @@
+export default function currentlyFetching(state=false, action) {
+    switch(action.type) {
+        case "CURRENTLY_FETCHING":
+            return true
+        case "SUCCESS_FETCHING":
+            return false
+        case "FAILED_FETCHING":
+            return false
+        default:
+            return false
+    }
+}
