2721526703bf6632963819764dccf17adfc181ba
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-19 21:25:11 -0700

    added major functionality on updating the qc files.

--- src/window-components/qcRecordWindow.js
+++ src/window-components/qcRecordWindow.js
@@ -9,47 +9,7 @@ export default class QCRecordWindow extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            qcFiles: [
-                {
-                    qcNum: "20001", 
-                    projectCode: "Release", 
-                    title: "UP456-1 release from Worldway",
-                    lotNums: ["FP101010", "FP101011", "FP101012"],
-                    tests: ["HPLC", "C&A", "LOD", "OSR", "GCMS"],
-                    dateIn: "01/23/2020",
-                    dateOut: "01/30/2020",
-                    nbPages: ["487p23", "490p17", "483p12-17"],
-                    requester: "QC",
-                    requesterName: null,
-                    finished: true
-                },
-                {
-                    qcNum: "20002", 
-                    projectCode: "Stability", 
-                    title: "UP456-1 T=12",
-                    lotNums: ["FP101014", "FP101015", "FP101016"],
-                    tests: ["HPLC", "C&A", "LOD", "OSR"],
-                    dateIn: "01/24/2020",
-                    dateOut: "01/31/2020",
-                    nbPages: ["487p27", "490p18", "483p17-18"],
-                    requester: "QC",
-                    requesterName: null,
-                    finished: true
-                },
-                {
-                    qcNum: "20003", 
-                    projectCode: "Project", 
-                    title: "AloReckt-1 Polysaccharides",
-                    lotNums: ["FP101019", "FP101020", "FP101021"],
-                    tests: ["HPLC-SEC"],
-                    dateIn: "02/01/2020",
-                    dateOut: "02/03/2020",
-                    nbPages: ["487p28", "490p20"],
-                    requester: "NP",
-                    requesterName: "Jordan P.",
-                    finished: true
-                }
-            ]
+            currQCFile: "20002",
         }
     }
 
@@ -71,25 +31,6 @@ export default class QCRecordWindow extends React.Component {
             })
     }
 
-    handlePostFiles = () => {
-        const params = {
-            headers:{},
-            response: true,
-            queryStringParameters: {},
-            body: {
-                num: "20002",
-                name: "Sample Name 2"
-            }
-        }
-
-        API.post("qcfilesAPI", "/qcfiles", params)
-            .then(response => {
-                console.log(response)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }
 
     handlePutFiles = () => {
         const params = {
@@ -145,6 +86,17 @@ export default class QCRecordWindow extends React.Component {
             })
     }
 
+    setNextQCFile = () => {
+        let currQCFile = this.state.currQCFile
+        let numberfied = parseInt(currQCFile, 10)
+        numberfied++
+        
+        console.log(numberfied)
+        this.setState({
+            currQCFile: numberfied.toString()
+        })
+    }
+
     render() {
         return (
             <div>
@@ -166,7 +118,7 @@ export default class QCRecordWindow extends React.Component {
                             <th>Update/Add/Delete</th>
                         </tr>
                         <tr>
-                            <QCRecordForm />
+                            <QCRecordForm currQCFile={this.state.currQCFile} setNextQCFile={this.setNextQCFile}/>
                         </tr>
                     </tbody>
                 </table>
@@ -176,7 +128,6 @@ export default class QCRecordWindow extends React.Component {
 
                 
                 <button onClick={this.handleGetFiles}> get files </button>
-                <button onClick={this.handlePostFiles}> post something</button>
                 <button onClick={this.handlePutFiles}> put something</button>
                 <button onClick={this.handleDeleteFiles}> delete something</button>
                 <button onClick={this.handleGetOneItem}> get one single item</button> 
