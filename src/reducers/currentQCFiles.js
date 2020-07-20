04130cedd1b90ba17b32fd700d4ffaddca6c6768
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-20 14:20:43 -0700

    renamed the action type.

--- src/reducers/currentQCFiles.js
+++ src/reducers/currentQCFiles.js
@@ -1,9 +1,9 @@
 export default function fetchStatus(state=[], action) {
     switch(action.type) {
-        case "GET_CURRENT_QC_FILES":
-            return action.payload
+        case "SET_CURRENT_QC_FILES":
+            return action.payload.sort((a,b) => a.num - b.num)
         case "ADD_NEW_QC_FILE":
-            return [...state, action.payload]
+            return [...state, action.payload].sort((a,b) => a.num - b.num)
         default:
             return state
     }
