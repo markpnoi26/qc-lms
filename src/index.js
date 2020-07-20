347bf82beb59ddfa6fc04b0ed839f109a0ce24c7
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-20 11:22:44 -0700

    applied redux to the application

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@ import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import App from './App';
 
+import {createStore, applyMiddleware, compose} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from './reducers/index'
+import thunk from 'redux-thunk'
+
 Amplify.configure(awsconfig);
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const qcStore = createStore(
+  reducer,
+  composeEnhancer(applyMiddleware(thunk)),
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={qcStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
