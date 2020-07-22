8f50544a55b023ce43a40a8e90731aee570b33c3
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-21 18:49:14 -0700

    major edits to how the Entry looks.

--- src/components/qcRecordEntry.js
+++ src/components/qcRecordEntry.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import {Button} from 'react-bootstrap'
+import {Button, ListGroup} from 'react-bootstrap'
+import QCEntryModal from '../modalComponent/qcEntryModal'
 
 
 export default class QcRecordEntry extends React.Component {
-    constructor() {
-        super()
-        this.state={}
+    constructor(props) {
+        super(props)
+        this.state={
+            modalShow: false
+        }
+    }
+
+    setModalShow = () => {
+        this.setState({
+            modalShow: !this.state.modalShow
+        })
     }
 
     render() {
@@ -21,64 +30,104 @@ export default class QcRecordEntry extends React.Component {
                     {this.props.file.title}
                 </td>
                 <td>
-                    <form>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    
-                                    <td>
-                                        <input type="checkbox" value="hplc" checked={this.props.file.tests.hplc} readOnly/>
-                                    </td>
-                                    <td>HPLC</td>
+                    <ListGroup horizontal>
+                        {Object.keys(this.props.file.tests).map((test, idx) => {
+                            if (this.props.file.tests[test]) {
+                                switch(test) {
+                                    case "colorAndAppearance":
+                                        return <ListGroup.Item key={test}> C&A </ListGroup.Item>
+                                    case 'lod':
+                                        return <ListGroup.Item key={test}> LOD </ListGroup.Item>
+                                    case 'ash':
+                                        return <ListGroup.Item key={test}> Ash </ListGroup.Item>
+                                    case 'particleSize':
+                                        return <ListGroup.Item key={test}> Particle Size </ListGroup.Item>
+                                    case 'solids':
+                                        return <ListGroup.Item key={test}> Solids </ListGroup.Item>
+                                    case 'odor':
+                                        return <ListGroup.Item key={test}> Odor </ListGroup.Item>
+                                    case 'meltingPoint':
+                                        return <ListGroup.Item key={test}> Melting Point </ListGroup.Item>
+                                    case 'ftir':
+                                        return <ListGroup.Item key={test}> FTIR </ListGroup.Item>
+                                    case 'nmr':
+                                        return <ListGroup.Item key={test}> NMR </ListGroup.Item>
+                                    case 'uvVis':
+                                        return <ListGroup.Item key={test}> UV-Vis/ </ListGroup.Item>
+                                    default :
+                                        return null
+                                }
 
-                                    <td>
-                                        <input type="checkbox" value="colorAndAppearance" checked={this.props.file.tests.colorAndAppearance} readOnly/>
-                                    </td>
-                                    <td>C&A</td>
-                                    <td>
-                                        <input type="checkbox" value="lod" checked={this.props.file.tests.lod} readOnly/>
-                                    </td>
-                                    <td>LOD</td>
-                                    
-                                </tr>
-                                <tr>
-                                    
-                                    <td>
-                                        <input type="checkbox" value="osr" checked={this.props.file.tests.osr} readOnly/>
-                                    </td>
-                                    <td>OSR</td>
-
-                                
-                                    <td>
-                                        <input type="checkbox" value="gcms" checked={this.props.file.tests.gcms} readOnly/>
-                                    </td>
-                                    <td>GC/MS</td>
-                                    
-                                    
-                                    <td>
-                                        <input type="checkbox" value="hptlc" checked={this.props.file.tests.hptlc} readOnly/>
-                                    </td>
-                                    <td>HPTLC</td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <input type="checkbox" value="uvVis" checked={this.props.file.tests.uvVis} readOnly/>
-                                    </td>
-                                    <td>UV-Vis</td>
-                                    <td>
-                                        <input type="checkbox" value="pesticides" checked={this.props.file.tests.pesticides} readOnly/>
-                                    </td>
-                                    <td>Pestic.</td>
-                                    <td>
-                                        <input type="checkbox" value="heavyMetals" checked={this.props.file.tests.heavyMetals} readOnly/>
-                                    </td>
-                                    <td>H/M</td>
-                                    
-                                </tr>
-                            </tbody>
-                        </table>
+                            }
+                            return <React.Fragment key={idx}></React.Fragment>
+                        })}
                         
-                    </form>
+                    </ListGroup>
+                    <ListGroup horizontal>
+                        {Object.keys(this.props.file.tests).map((test, idx) => {
+                            if (this.props.file.tests[test]) {
+                                switch(test) {
+                                    case "hplc":
+                                        return <ListGroup.Item key={test}> HPLC </ListGroup.Item>
+                                    case "gcms":
+                                        return <ListGroup.Item key={test}> GC/MS </ListGroup.Item>
+                                    case "hptlc":
+                                        return <ListGroup.Item key={test}> HPTLC </ListGroup.Item>
+                                    case "sec":
+                                        return <ListGroup.Item key={test}> SEC </ListGroup.Item>
+                                    default :
+                                        return null
+                                }
+
+                            }
+                            return <React.Fragment key={idx}></React.Fragment>
+                        })}
+                        
+                    </ListGroup>
+                    <ListGroup horizontal>
+                        {Object.keys(this.props.file.tests).map((test, idx) => {
+                            if (this.props.file.tests[test]) {
+                                switch(test) {
+                                    case "totalPlateCount":
+                                        return <ListGroup.Item key={test}> TPC </ListGroup.Item>
+                                    case "coliform":
+                                        return <ListGroup.Item key={test}> Coliform </ListGroup.Item>
+                                    case "yeastAndMold":
+                                        return <ListGroup.Item key={test}> Y&M </ListGroup.Item>
+                                    case "eColi":
+                                        return <ListGroup.Item key={test}> E.Coli </ListGroup.Item>
+                                    case "salmonella":
+                                        return <ListGroup.Item key={test}> Salmonella </ListGroup.Item>
+                                    default :
+                                        return null
+                                }
+
+                            }
+                            return <React.Fragment key={idx}></React.Fragment>
+                        })}
+                        
+                    </ListGroup>
+                    <ListGroup horizontal>
+                        {Object.keys(this.props.file.tests).map((test, idx) => {
+                            if (this.props.file.tests[test]) {
+                                switch(test) {
+                                    case "arsenic":
+                                        return <ListGroup.Item key={test}> As (HM) </ListGroup.Item>
+                                    case "lead":
+                                        return <ListGroup.Item key={test}> Pb (HM)</ListGroup.Item>
+                                    case "mercury":
+                                        return <ListGroup.Item key={test}> Hg (HM) </ListGroup.Item>
+                                    case "cadmium":
+                                        return <ListGroup.Item key={test}> Cd (HM) </ListGroup.Item>
+                                    default :
+                                        return null
+                                }
+
+                            }
+                            return <React.Fragment key={idx}></React.Fragment>
+                        })}
+                        
+                    </ListGroup>
                 </td>
                 <td>
                     <ol>
@@ -109,9 +158,18 @@ export default class QcRecordEntry extends React.Component {
                 </td>
                     
                 <td style={{textAlign:"center"}}>
-                    <Button variant="secondary"> Update </Button>
-                    <Button variant="danger"> Delete </Button>
+                    <Button 
+                        variant="secondary" 
+                        onClick={() => this.setModalShow()}> 
+                        Info Card 
+                    </Button>
                 </td>
+
+                <QCEntryModal 
+                    show={this.state.modalShow}
+                    onHide={() => this.setModalShow()}
+                />
+
             </>
         )
     }
