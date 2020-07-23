3680bb1684cb70e2aa5d053a9c3d898ec4ad1e23
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-23 11:03:44 -0700

    added a new case to reducer for updates

--- src/reducers/currentQCFiles.js
+++ src/reducers/currentQCFiles.js
@@ -1,9 +1,11 @@
-export default function fetchStatus(state=[], action) {
+export default function currenQCFiles(state=[], action) {
     switch(action.type) {
         case "SET_CURRENT_QC_FILES":
             return action.payload.sort((a,b) => a.num - b.num)
         case "ADD_NEW_QC_FILE":
             return [...state, action.payload].sort((a,b) => a.num - b.num)
+        case "UPDATE_QC_FILES":
+            return action.payload.sort((a,b) => a.num - b.num)
         default:
             return state
     }
