2613286f3b1608085d0c179d49bed3a2d7631fb3
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-22 18:14:05 -0700

    cleaned up to have a NavBar

--- src/App.js
+++ src/App.js
@@ -2,26 +2,18 @@ import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
 import {Container, Row, Col} from 'react-bootstrap'
 import TopBar from './containers/topBar'
-import SideBar from './containers/sideBar'
 import MainWindow from './containers/mainWindow'
 import {withAuthenticator} from '@aws-amplify/ui-react';
-import "./styles/sidebar.scss"
 
 class App extends React.Component {
 
   render() {
     return (
         <Container style={{fontSize: 12}} fluid >
-          <Row>
-            <TopBar />
-          </Row>
           <Router>
             <Row>
-              <Col xs={2} id="sidebar-wrapper">
-                <SideBar />
-              </Col>
-
-              <Col xs={10} id="page-content-wrapper">
+              <Col>
+                <TopBar />
                 <MainWindow />
               </Col>
             </Row>
