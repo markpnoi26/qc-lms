8de064c65fee7ee7f196959ce92b4b8a5a8fd931
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-23 12:51:56 -0700

    added a new component for edits.

---
+++ src/components/editTitleTypeLot.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Form} from 'react-bootstrap'
+
+export default class EditTitleTypeLot extends React.Component {
+    
+
+    render() {
+        return(
+            <Form>
+                hello
+            </Form>
+        )
+    }
+}
