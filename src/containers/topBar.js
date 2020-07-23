ad12f4e5190583656190fa1152253ff3ea30e8b9
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-22 18:15:04 -0700

    renamed and reconfigured navbar

--- src/containers/topBar.js
+++ src/containers/topBar.js
@@ -1,17 +1,53 @@
-import React from 'react'
-import {Col} from 'react-bootstrap'
+import React from "react";
+import {Nav, Navbar, Form, FormControl,Button} from "react-bootstrap";
+import {Link} from 'react-router-dom'
+import { withRouter } from "react-router";
+import {Auth} from 'aws-amplify';
 
-export default class TopBar extends React.Component {
 
-    render() {
-        
-        return (
-            <React.Fragment>
-                <Col >
-                    <h1>QC-LMS</h1>
-                </Col>
-            </React.Fragment>
-            
-        )
+async function signOut() {
+    try {
+        await Auth.signOut();
+        window.location.reload(false);
+    } catch (error) {
+        console.log('error signing out: ', error);
     }
 }
+
+const Top = props => {
+
+    return (
+        <Navbar 
+            expand="lg"
+        >   
+            <Navbar.Brand>QC-LMS</Navbar.Brand>
+            <Nav
+                variant="tabs"
+                fill
+                className="mr-auto"
+            >
+                <Nav.Item>
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link as={Link} to="/records">QC Records</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link as={Link} to="/retain">QC Retains</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link as={Link} to="/stability" >Stability</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <Nav className="mr-sm-2">
+            
+                <Button onClick={signOut} variant="outline-danger" >SignOut</Button>
+            </Nav>
+            
+        </Navbar>
+        
+        );
+  };
+  const TopBar = withRouter(Top);
+
+  export default TopBar
