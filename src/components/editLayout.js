2dff2c833ec7eb063607f642948ad73442ab60f8
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-23 19:00:58 -0700

    started editing the form

--- src/components/editLayout.js
+++ src/components/editLayout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Form} from 'react-bootstrap'
+import {Form, Row, Col, Container} from 'react-bootstrap'
 
 export default class EditLayout extends React.Component {
     
@@ -7,7 +7,24 @@ export default class EditLayout extends React.Component {
     render() {
         return(
             <Form>
-                hello
+                <Container>
+                    <Row>
+                        <Col>
+                            <Form.Control
+                                as="select"
+                                size="sm">
+                                    <option value="">Select Type</option>
+                                    <option value="P">(P) Project</option>
+                                    <option value="S">(S) Stability</option>
+                                    <option value="I">(I) Investigation</option>
+                                    <option value="R">(I) Release</option>
+                                    <option value="SQ">(SQ) Standard Qualification</option>
+
+                            </Form.Control>
+                                
+                        </Col>
+                    </Row>
+                </Container>
             </Form>
         )
     }
