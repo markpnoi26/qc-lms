34584cb335180dce999e82a1ab1d1cb5332b1790
Mark <markpnoi26@gmail.com>
2020-07-25 12:48:52 -0700

    cleaned up this component to not have any internal state

--- src/components/editLayoutForm.js
+++ src/components/editLayoutForm.js
@@ -1,17 +1,12 @@
 import React from 'react'
-import {Form, Row, Col, Container} from 'react-bootstrap'
+import {Form, Row, Col, Container, InputGroup, FormControl, Button} from 'react-bootstrap'
+import {v4 as uuidv4} from 'uuid'
+import {DashCircleFill} from 'react-bootstrap-icons'
 
 export default class EditLayoutForm extends React.Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            currLotNum: ""
-        }
-    }
-
     render() {
-        let { projectType, title, tests, lotNums, dateIn, dateOut, requester, analyst, notes, nbPage } = this.props
+        let { projectType, title, tests, lotNums, dateIn, dateOut, requester, analyst, nbPage, currLotNum, handleProjectInfoChange, handleProjectTextChange, handleAddNewLot, handleDeleteLot} = this.props
         return(
             <Container>
                 <Row>
@@ -21,7 +16,7 @@ export default class EditLayoutForm extends React.Component {
                                 as="select"
                                 size="sm"
                                 label="projectType"
-                                onChange={this.props.handleProjectInfoChange}
+                                onChange={handleProjectInfoChange}
                                 value={projectType}>
                                     <option value="">Select Project Type</option>
                                     <option value="P">(P) Project</option>
@@ -39,7 +34,7 @@ export default class EditLayoutForm extends React.Component {
                                 size="sm"
                                 label="requester"
                                 value={requester}
-                                onChange={this.props.handleProjectInfoChange}>
+                                onChange={handleProjectInfoChange}>
                                     <option value="">Select Requester</option>
                                     <option value="QC">QC</option>
                                     <option value="NP">NP</option>
@@ -54,7 +49,7 @@ export default class EditLayoutForm extends React.Component {
                                 size="sm"
                                 label="analyst"
                                 value={analyst}
-                                onChange={this.props.handleProjectInfoChange}>
+                                onChange={handleProjectInfoChange}>
                                     <option value="">Select Analyst</option>
                                     <option value="MD">MD</option>
                                     <option value="KH">KH</option>
@@ -65,7 +60,46 @@ export default class EditLayoutForm extends React.Component {
                 </Row>
                 <Row>
                     <Form>
+                        <Col>
                         
+                        </Col>
+                        <Col>
+                        
+                        </Col> 
+                        <Col>
+                            <Container>
+                                <Row>
+                                    <InputGroup >
+                                        <FormControl
+                                            type="text"
+                                            size="sm"
+                                            value={currLotNum}
+                                            label="currLotNum"
+                                            placeholder="New Lot"
+                                            onChange={handleProjectTextChange}
+                                        />
+                                        <InputGroup.Append>
+                                            <Button size="sm" variant="outline-success" onClick={handleAddNewLot} >+</Button>
+                                        </InputGroup.Append>
+                                    </InputGroup>
+                                </Row>
+                            </Container>
+                            <Container>
+                                {lotNums.map((lot) => (
+                                    <Row key={uuidv4()}>
+                                        <Col>
+                                            {lot}
+                                        </Col> 
+                                        <Col>
+                                            <DashCircleFill size="11" color="red" style={{ cursor: "pointer" }} onClick={handleDeleteLot} value={"lot"} />
+                                        </Col>
+                                        
+                                    </Row>
+                                
+                                    ))
+                                }
+                            </Container>    
+                        </Col>  
                     </Form>
                 </Row>
             </Container>
