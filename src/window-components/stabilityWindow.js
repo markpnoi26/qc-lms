15acb155aa3fe0d0073962dbf51b8fccbd25ce8d
Mark <markpnoi26@gmail.com>
2020-07-27 19:36:50 -0700

    started some edits to the stability window

--- src/window-components/stabilityWindow.js
+++ src/window-components/stabilityWindow.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Table} from 'react-bootstrap'
 
 export default class StabilityWindow extends React.Component {
 
@@ -6,7 +7,22 @@ export default class StabilityWindow extends React.Component {
     render() {
         return (
             <div>
-                Currently under Stability Window
+                <Table bordered striped variant="dark" >
+                    <thead>
+                        <tr style={{ textAlign: "center" }} >
+                            <th>Protocol Number</th>
+                            <th>Project Title</th>
+                            <th>lot #</th>
+                            <th>Condition</th>
+                            <th>Date Started</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody >
+                        <tr style={{ textAlign: "center" }}>
+                        </tr>
+                    </tbody>
+                </Table>
             </div>
         )
     }
