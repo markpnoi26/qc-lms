fedffd5ad59b6cc4af0afccc795539cdc9575294
Mark <markpnoi26@gmail.com>
2020-07-27 19:36:38 -0700

    started editing retain window

--- src/window-components/retainWindow.js
+++ src/window-components/retainWindow.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Table} from 'react-bootstrap'
 
 export default class RetainWindow extends React.Component {
 
@@ -6,8 +7,25 @@ export default class RetainWindow extends React.Component {
     render() {
         return (
             <div>
-                Currently under Retain Window
+                <Table bordered striped variant="dark" >
+                    <thead>
+                        <tr style={{ textAlign: "center" }} >
+                            <th>QC Number</th>
+                            <th>Project Type</th>
+                            <th>Project Title</th>
+                            <th>lot #</th>
+                            <th>Sample #</th>
+                            <th>Date Started</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody >
+                        <tr style={{ textAlign: "center" }}>
+                        </tr>
+                    </tbody>
+                </Table>
             </div>
+
         )
     }
 }
