c51d76be2160fbe022b3454f1ae59ef04e396ea9
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-07-28 18:30:03 -0700

    added state to this component with loading overlay

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
-import {Container, Row, Col} from 'react-bootstrap'
+import {Container, Row, Col, Spinner} from 'react-bootstrap'
+import {withAuthenticator} from '@aws-amplify/ui-react';
+import {connect} from 'react-redux'
 import TopBar from './containers/topBar'
 import MainWindow from './containers/mainWindow'
-import {withAuthenticator} from '@aws-amplify/ui-react';
+import LoadingOverlay from 'react-loading-overlay';
+
 
 class App extends React.Component {
 
   render() {
     return (
+      <LoadingOverlay 
+        active={this.props.fetchStatus}
+        spinner={ <Spinner animation="border" variant="info" />}
+        >
         <Container style={{fontSize: 14}} fluid >
           <Router>
             <Row>
               <Col>
+               
                 <TopBar />
                 <MainWindow />
+                
               </Col>
             </Row>
           </Router>
         </Container>
+      </LoadingOverlay>
     );
   }
 
 }
 
-export default withAuthenticator(App);
+const mapStateToProps = state => {
+  return {
+    fetchStatus: state.fetchStatus
+  }
+}
+
+export default withAuthenticator(connect(mapStateToProps, null)(App));
