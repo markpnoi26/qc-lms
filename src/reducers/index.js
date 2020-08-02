e8ae70b13f7061893392cf3cf5ddc1ecf248dd36
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-08-01 22:17:00 -0700

    added new reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,15 @@ import currentYear from './currentYear'
 import currentQCFiles from './currentQCFiles'
 import fetchStatus from './fetchStatus'
 import currentAvailableQCFile from './currentAvailableQCFile'
+import currentAvailableStabilityProtocol from './currentAvailableStabilityProtocol'
+import currentStabilityProtocol from './currentStabilityProtocol'
 
 
 export default combineReducers({
     currentYear,
     currentQCFiles,
+    currentStabilityProtocol,
     currentAvailableQCFile,
+    currentAvailableStabilityProtocol,
     fetchStatus
 })
