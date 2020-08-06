b208655a373e94d3d6c02f227f3e82752ac6a9ca
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-08-06 14:26:35 -0700

    renamed this window

--- src/containers/window-containers/homeWindow.js
+++ src/containers/window-containers/homeWindow.js
@@ -6,7 +6,7 @@ export default class HomeWindow extends React.Component {
     render() {
         return (
             <div>
-                Currently under Home Window
+                The Home Window is Under Construction
             </div>
         )
     }
