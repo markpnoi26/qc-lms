4aa0aa1cc36cf54a03454817898f3c1c57446442
Mark <markpnoi26@gmail.com>
2020-08-08 20:07:26 -0700

    added curent active window

--- src/containers/window-containers/homeWindow.js
+++ src/containers/window-containers/homeWindow.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
-export default class HomeWindow extends React.Component {
+class HomeWindow extends React.Component {
 
+    componentDidMount = () => {
+        this.props.setCurrentActiveWindow("home")
+    }
 
     render() {
         return (
@@ -11,3 +15,17 @@ export default class HomeWindow extends React.Component {
         )
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        currentActiveWindow: state.currentActiveWindow
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        setCurrentActiveWindow: (window) => dispatch({ type: "SET_CURRENT_ACTIVE_WINDOW", payload: window })
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeWindow)
