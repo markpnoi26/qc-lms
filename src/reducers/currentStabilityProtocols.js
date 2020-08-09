ae6fc35898d808448225b42044952f2aa4902854
Mark <markpnoi26@gmail.com>
2020-08-08 20:09:31 -0700

    renamed the string case.

--- src/reducers/currentStabilityProtocols.js
+++ src/reducers/currentStabilityProtocols.js
@@ -1,6 +1,6 @@
 export default function currentStabilityProtocols(state=[], action) {
     switch(action.type) {
-        case "SET_CURRENT_STABILITY_PROTOCOL":
+        case "SET_CURRENT_STABILITY_PROTOCOLS":
             return action.payload.sort((a,b) => a.num - b.num)
         case "ADD_NEW_STABILITY_PROTOCOL":
             return [...state, action.payload].sort((a,b) => a.num - b.num)
