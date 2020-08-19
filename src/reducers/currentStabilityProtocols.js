6a390618690ca933f9b16595329f0523561b4072
Mark <markpnoi26@gmail.com>
2020-08-19 16:15:23 -0700

    updated reducer to sort by correct key.

--- src/reducers/currentStabilityProtocols.js
+++ src/reducers/currentStabilityProtocols.js
@@ -1,11 +1,11 @@
 export default function currentStabilityProtocols(state=[], action) {
     switch(action.type) {
         case "SET_CURRENT_STABILITY_PROTOCOLS":
-            return action.payload.sort((a,b) => a.num - b.num)
+            return action.payload.sort((a,b) => a.stabilityProtocolNum - b.stabilityProtocolNum)
         case "ADD_NEW_STABILITY_PROTOCOL":
-            return [...state, action.payload].sort((a,b) => a.num - b.num)
+            return [...state, action.payload].sort((a,b) => a.stabilityProtocolNum - b.stabilityProtocolNum)
         case "UPDATE_STABILITY_PROTOCOL":
-            return action.payload.sort((a,b) => a.num - b.num)
+            return action.payload.sort((a,b) => a.stabilityProtocolNum - b.stabilityProtocolNum)
         default:
             return state
     }
