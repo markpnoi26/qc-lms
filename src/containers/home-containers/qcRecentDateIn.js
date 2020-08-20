b24a8f14d9e9b46ba819f255a5457dc331800c7a
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-08-20 15:06:44 -0700

    added new component for date in

---
+++ src/containers/home-containers/qcRecentDateIn.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class QCRecentDateIn extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentlyFetching: false
+        }
+    }
+
+    render() {
+        return (
+            <div>In</div>
+        )
+    }
+}
