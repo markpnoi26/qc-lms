bcee441224201e9828c343302d2360a1e3c39eac
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-08-20 15:06:54 -0700

    added new component date out

---
+++ src/containers/home-containers/qcRecentDateOut.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class QCRecentDateOut extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentlyFetching: false
+        }
+    }
+
+    render() {
+        return (
+            <div>Out</div>
+        )
+    }
+}
