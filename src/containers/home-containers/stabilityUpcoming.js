f9c12b2935a66dac7845536bc2b2c265cf1985d8
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-08-20 15:07:07 -0700

    added new component for upcoming stability

---
+++ src/containers/home-containers/stabilityUpcoming.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class StabilityUpcoming extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentlyFetching: false
+        }
+    }
+
+    render() {
+        return (
+            <div>Stability Upcoming</div>
+        )
+    }
+}
