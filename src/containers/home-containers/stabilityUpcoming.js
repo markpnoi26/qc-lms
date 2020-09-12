78b30a62d44ecd129139133b30b979bd1c7dcd23
Mark <markpnoi26@gmail.com>
2020-09-12 12:54:25 -0700

    added component Mount API call

--- src/containers/home-containers/stabilityUpcoming.js
+++ src/containers/home-containers/stabilityUpcoming.js
@@ -5,12 +5,35 @@ export default class StabilityUpcoming extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            currentlyFetching: false
+            currentlyFetching: false,
+            stabilityProtocol: []
         }
     }
 
     componentDidMount = () => {
 
+        const currDate = new Date()
+        const currYear = currDate.getUTCFullYear()
+        const prevYear = currYear - 5
+
+        const params = {
+            headers: {},
+            response: true,
+            queryStringParameters: {
+                range: prevYear
+            }
+        }
+
+        this.setState({currentlyFetching: true})
+        API.get("stabilityAPI", "/stability", params)
+            .then(response => {
+                this.setState({ currentlyFetching: false })
+                this.setState({ stabilityProtocol: response.data})
+            })
+            .catch(error => {
+                this.setState({ currentlyFetching: false })
+                console.log({ error })
+            })
     }
 
     render() {
