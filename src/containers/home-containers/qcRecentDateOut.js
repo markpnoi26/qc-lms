065a0acb7a9fee266b4c43d4be731eb9b3e8f122
Mark <markpnoi26@gmail.com>
2020-09-11 18:15:52 -0700

    established out

--- src/containers/home-containers/qcRecentDateOut.js
+++ src/containers/home-containers/qcRecentDateOut.js
@@ -1,16 +1,74 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { API } from 'aws-amplify'
 
-export default class QCRecentDateOut extends React.Component {
+class QCRecentDateOut extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            currentlyFetching: false
+            windowFetching: false,
+            qcFiles: []
         }
+    }
+
+    componentDidMount = () => {
+        const currDate = new Date()
+        const currYear = currDate.getUTCFullYear()
+
+        const params = {
+            headers: {},
+            response: true,
+            queryStringParameters: {
+                currentYear: this.props.currentYear || currYear
+            }
+        }
+
+        this.setState({ windowFetching: true })
+        API.get("qcfilesAPI", "/qcfiles", params)
+            .then(response => {
+
+                response.data.sort((a, b) => {
+                    return (b.dateIn.length - a.dateIn.length)
+                })
+
+                response.data.sort((a, b) => {
+                    return (new Date(b.dateOut) - new Date(a.dateOut))
+                })
+
+                this.setState({
+                    windowFetching: false,
+                    qcFiles: response.data
+                })
+            })
+            .catch(error => {
+                this.setState({ windowFetching: false })
+                console.log(error)
+            })
     }
 
     render() {
         return (
-            <div>Out</div>
+            <div>
+                <h1>Latest QC Files Out: </h1>
+                <ul>
+                    {this.state.qcFiles.slice(0, 15).map(file => <li>QC{file.num} -  {file.title}</li>)}
+                </ul>
+            </div>
         )
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        currentQCFiles: state.currentQCFiles,
+        currentYear: state.currentYear
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        setCurrentQCFiles: (qcFiles) => dispatch({ type: "SET_CURRENT_QC_FILES", payload: qcFiles })
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(QCRecentDateOut)
